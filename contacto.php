8c21e39fe38c5426794fbc400fb96a88e3bc746b
Cleste2004 <glestrange92@gmail.com>
2021-08-19 14:19:02 -0300

    cambios

--- contacto.php
+++ contacto.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap"
         rel="stylesheet">
 
@@ -25,17 +25,17 @@
               <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                   <li class="nav-item">
-                    <a class="nav-link active pe-4" href="index.html">Inicio</a>
+                    <a class="nav-link active pe-4" href="index.php">Inicio</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active pe-4" href="sobre-mi.html">Sobre mi</a>
+                    <a class="nav-link active pe-4" href="sobre-mi.php">Sobre mi</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active pe-4" href="proyectos.html">Proyectos</a>
+                    <a class="nav-link active pe-4" href="proyectos.php">Proyectos</a>
                    
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active" href="contacto.html">Contacto</a>
+                    <a class="nav-link active" href="contacto.php">Contacto</a>
                   </li>
                 </ul>
                 <form class="d-flex">
@@ -60,6 +60,7 @@
             <div class="row">
                 <div class="col-12 col-sm-6">
                     <p>Te invito a que te contactes enviándome un mensaje o bien por whatsapp.</p>
+                    <img src="imagenes/QR whatsapp.png">
 
                 </div>
                 <div class="col-12 col-sm-6">
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap"
         rel="stylesheet">
 
@@ -25,17 +25,17 @@
               <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                   <li class="nav-item">
-                    <a class="nav-link active pe-4" href="index.html">Inicio</a>
+                    <a class="nav-link active pe-4" href="index.php">Inicio</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active pe-4" href="sobre-mi.html">Sobre mi</a>
+                    <a class="nav-link active pe-4" href="sobre-mi.php">Sobre mi</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active pe-4" href="proyectos.html">Proyectos</a>
+                    <a class="nav-link active pe-4" href="proyectos.php">Proyectos</a>
                    
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active" href="contacto.html">Contacto</a>
+                    <a class="nav-link active" href="contacto.php">Contacto</a>
                   </li>
                 </ul>
                 <form class="d-flex">
@@ -62,13 +62,13 @@
     </div>
       <div class="row">
           <div class="col-12 mt-4 text-center">
-              <a href="proyectos.html"><img src="imagenes/imagen fondo 1.png" class="circulo"></a>
+              <a href="proyectos.php"><img src="imagenes/imagen fondo 1.png" class="circulo"></a>
           </div>
       </div>
       
       <div class="row  btn4">
           <div class="col-12 text-center">
-              <a href="proyectos.html" class="btn3">Conoce mis proyectos</a>
+              <a href="proyectos.php" class="btn3">Conoce mis proyectos</a>
           </div>
       </div>
   </main>
